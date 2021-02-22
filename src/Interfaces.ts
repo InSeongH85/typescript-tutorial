b2bc28917d193a4a21fcd94d8201aef1a8b764d8
InSeongH85 <tanbbang01@gmail.com>
2021-02-22 18:02:59 +0900

    Add interface

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -14,3 +14,38 @@ function printSize(labeledObj: LabeledValue) {
 let myObj = { size: 10, label: 'Size 10 Object' };
 printLabel(myObj);
 printSize(myObj);
+
+interface SquareConfig {
+	color?: string;
+	width?: number;
+}
+
+function createSquare(config: SquareConfig): { color: string; area: number } {
+	let newSquare = { color: 'white', area: 100 };
+	if (config.color) {
+		// Error: Property 'clor' does not exist on type 'SquareConfig'
+		newSquare.color = config.color;
+	}
+	if (config.width) {
+		newSquare.area = config.width * config.width;
+	}
+	return newSquare;
+}
+
+let blackSquare = createSquare({ color: 'black', width: 10 });
+console.log(blackSquare);
+
+let blueSquare = createSquare({ color: 'blue', width: 11 });
+console.log(blueSquare);
+
+/**
+ * Readonly Properties
+ * */
+interface Point {
+	readonly x: number;
+	readonly y: number;
+}
+
+let p1: Point = { x: 10, y: 20 };
+//p1.x = 5; // error!
+//Cannot assign to 'x' because it is a read-only property.
